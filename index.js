2954e79f40ba41cdf22395b74a8079321152316a
Timotius Anrez <71978054+TimotiusAnrez@users.noreply.github.com>
2022-07-26 12:34:38 +0700

    fibonaci explained

--- index.js
+++ index.js
@@ -4,6 +4,17 @@
  *
  * definisi: angka ke n = f(n-1) + f(n-2)
  * jadi angka ke n dapetnya dari angka sebelumnya ditambah 2 angka sebelumnya
+ *
+ *
+ * ? penjelasan untuk hansen
+ *
+ * * jadi fibonaci itu pada intinya adalah angka yang dimana hasil dari angka sebelumnya ditambah sama 2 angka sebelumnya
+ *
+ * ! contoh angka fibonaci ke 2 adalah 1
+ * ! datengnya dari 1 + 0
+ * ! 1 itu adalah angka sebelum fibonaci posisi ke 2
+ * ! 0 itu adalah 2 angka sebelum fibonaci posisi ke 2
+ * ! jadi fibonaci posisi ke 2 adalah 1 + 0 = 1
  */
 
 function fibo(n) {
