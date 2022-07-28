2e2c41713c2fcc0e0a4685ec1c07c79628e67712
Timotius Anrez <71978054+TimotiusAnrez@users.noreply.github.com>
2022-07-28 19:28:54 +0700

    variable exercise1

---
+++ variable/exercise/index.js
@@ -0,0 +1,12 @@
+function exercise1() {
+	let a = 1;
+	let b = 2;
+	let c = 3;
+
+	//jangan ubah kode diatas
+
+	//jangan ubah return value
+	return [a, b, c];
+}
+
+console.log(exercise1()); //hasil harus berbentuk ini [10,20,30]
