32eb4a1ce573773466d5699d4f5a1c4c9e87129c
unknown <hansen.permadi@binus.ac.id>
2022-08-05 10:15:09 +0700

    saya masih harus banyak belajar

--- variable/hansenExercise/exercise2.js
+++ variable/hansenExercise/exercise2.js
@@ -3,13 +3,14 @@ function exercise2(arg1, arg2, arg3) {
 
 	//kode diatas tidak boleh diubah
 
-	num1 = 20;
-	number2 = 30;
-	nomor3 = 20;
+	num1 = arg1;
+	number2 = arg2;
+	nomor3 = arg3;
 
 	//kode dibawah tidak boleh diubah
 
 	return [num1, number2, nomor3]; //hasil adalah [20,30,20]
 }
-console.log(exercise2(num1, number2, nomor3));
+
+console.log(exercise2(20, 30, 20));
 module.exports = exercise2;
