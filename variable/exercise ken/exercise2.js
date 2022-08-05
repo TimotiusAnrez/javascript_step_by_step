0c153a21c7b831ee0ecfaaaa99404279729dcba6
Kenji <109609000+kenjiark@users.noreply.github.com>
2022-08-05 20:30:59 +0700

    ini kenji

---
+++ variable/exercise ken/exercise2.js
@@ -0,0 +1,15 @@
+function exercise2(arg1, arg2, arg3) {
+	let num1, number2, nomor3;
+
+	//kode diatas tidak boleh diubah
+
+	num1 = arg1;
+	number2 = arg2;
+	nomor3 = arg3;
+
+	//kode dibawah tidak boleh diubah
+
+	return [num1, number2, nomor3]; //hasil adalah [20,30,20]
+}
+console.log(exercise2(20,30,20));
+module.exports = exercise2;
